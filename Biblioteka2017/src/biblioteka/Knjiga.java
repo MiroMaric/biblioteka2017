3e691f7d14ab93f6f3f870b71ff9b3d0453d7839
MiroMaric <miromaric1996@gmail.com>
2017-03-15 15:39:20 +0100

    Dodate equals i toString memote za klase Autori i Knjiga. Izmenjane geteri i seteri za Knjigu.

--- Biblioteka2017/src/biblioteka/Knjiga.java
+++ Biblioteka2017/src/biblioteka/Knjiga.java
@@ -8,7 +8,7 @@ public class Knjiga {
 	private long isbn;
 	private String izdavac;
 	private int izdanje;
-	private LinkedList<Autor> autori;
+	public LinkedList<Autor> autori;
 	public String getNaslov() {
 		return naslov;
 	}
@@ -33,6 +33,32 @@ public class Knjiga {
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
+	public LinkedList<Autor> getIzdavaci(){
+		return autori;
+	}
+	public void setAutori(LinkedList<Autor> autori){
+		this.autori = autori;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", izdavac=" + izdavac + ", izdanje=" + izdanje
+				+ ", autori=" + autori + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (isbn != other.isbn)
+			return false;
+		return true;
+	}
+	
+	
 
 	
 	
