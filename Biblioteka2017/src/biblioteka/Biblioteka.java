c089c49d89f666d71538775cb3d1209ac5235aa0
MiroMaric <miromaric1996@gmail.com>
2017-03-22 14:49:54 +0100

    Uvedene logicka kontrola

--- Biblioteka2017/src/biblioteka/Biblioteka.java
+++ Biblioteka2017/src/biblioteka/Biblioteka.java
@@ -5,29 +5,43 @@ import java.util.LinkedList;
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
-
+	
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
 	@Override
 	public void dodajKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
+		if(k==null)
+			throw new RuntimeException("Knjiga je null");
+		knjige.add(k);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
+		if(k==null)
+			throw new RuntimeException("Knjiga je null");
+		knjige.remove(k);
 
 	}
 
 	@Override
 	public LinkedList<Knjiga> bratiSveKnjige() {
-		// TODO Auto-generated method stub
-		return null;
+		return knjige;
 	}
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, long isbn, String izdavac) {
-		// TODO Auto-generated method stub
-		return null;
+		if(autor==null || isbn == 0 || naslov == null || izdavac == null)
+			throw new RuntimeException("Morate uneti neki kriterijum");
+		
+		LinkedList<Knjiga> rezultati = new LinkedList<Knjiga>();
+		
+		for(int i=0;i<knjige.size();i++){
+			if(knjige.get(i).getNaslov().contains(naslov))
+				rezultati.add(knjige.get(i));
+		}
+		
+		return rezultati;
 	}
 
 }
