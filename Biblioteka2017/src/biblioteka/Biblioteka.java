60c21aae986d78c5cac7cad6b7f21832d167dda1
MiroMaric <miromaric1996@gmail.com>
2017-03-22 14:54:48 +0100

    Promenjena logika u pronadji metodi

--- Biblioteka2017/src/biblioteka/Biblioteka.java
+++ Biblioteka2017/src/biblioteka/Biblioteka.java
@@ -5,29 +5,39 @@ import java.util.LinkedList;
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
-
+	
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
 	@Override
 	public void dodajKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
+		knjige.add(k);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
+		knjige.remove(k);
 
 	}
 
 	@Override
 	public LinkedList<Knjiga> bratiSveKnjige() {
-		// TODO Auto-generated method stub
-		return null;
+		return knjige;
 	}
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, long isbn, String izdavac) {
-		// TODO Auto-generated method stub
-		return null;
+		if(autor==null || isbn == 0 || naslov == null || izdavac == null)
+			return null;
+		
+		LinkedList<Knjiga> rezultati = new LinkedList<Knjiga>();
+		
+		for(int i=0;i<knjige.size();i++){
+			if(knjige.get(i).getNaslov().contains(naslov))
+				rezultati.add(knjige.get(i));
+		}
+		
+		return rezultati;
 	}
 
 }
